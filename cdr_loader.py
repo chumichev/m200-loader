476333de16d6c20a3314a2275755ef192fd48144
Pavel Chumichev <pavel.chumichev@gmail.com>
2021-07-29 20:25:55 +0300

    Отлажено взаимодействие основного процесса с коллектором

--- cdr_loader.py
+++ cdr_loader.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 import logging
-import sys
+import queue
 from m200_collector import M200Collector
 from multiprocessing import Queue
 
 
 def main():
     cdr_queue = Queue()
-    online_collector = M200Collector(host='10.90.254.116', port=20002, output_queue=cdr_queue)
+    online_collector = M200Collector(host='10.90.254.115', port=20002, output_queue=cdr_queue)
     online_collector.start()
 
-    while True:
-        # TODO: Разобраться с выходом из цикла если процесс мёртв
-        if not online_collector.is_alive():
-            break
-        cdr = cdr_queue.get()
-        print(cdr)
+    while online_collector.is_alive():
+        try:
+            cdr = cdr_queue.get(timeout=1)
+            print(cdr)
+        except queue.Empty:
+            # Здесь ничего не нужно делать. Очередь пустая? - проверяем жив ли процесс коллектор.
+            # Если жив, то снова ждём очередь и так по кругу. Если коллектор умер, то завершаем приложение
+            pass
 
     online_collector.join()
 
--- m200_collector.py
+++ m200_collector.py
@@ -9,46 +9,61 @@ from time import sleep
 
 class M200Collector(Process):
 
+    CONNECTION_TIMEOUT = 5
+
     def __init__(self, host: str, port: int, output_queue: Queue,
                  buffer_size=1024, encoding='cp1251', reconnect_timeout=2, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.__buffer_size = buffer_size
-        self.__encoding = encoding
-        self.__output_queue = output_queue
-        self.__m200_address = (host, port)
-        self.__tcp_socket = None
-        self.__connected = False
-        self.__reconnect_timeout = reconnect_timeout
+        self._buffer_size = buffer_size
+        self._encoding = encoding
+        self._output_queue = output_queue
+        self._m200_address = (host, port)
+        self._tcp_socket = None
+        self._connected = False
+        self._reconnect_timeout = reconnect_timeout
 
-    def __connect(self):
-        self.__tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.__tcp_socket.connect(self.__m200_address)
-        self.__connected = True
+    def _setup_socket(self):
+        """
+        Установка параметров сокета (без подключения)
+        """
+        self._tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._tcp_socket.settimeout(self.CONNECTION_TIMEOUT)
+
+    def _connect(self):
+        """
+        Подключает сокет на адрес и порт переданные в _m200_address
+        """
+        self._setup_socket()
+        self._tcp_socket.connect(self._m200_address)
+        self._connected = True
 
     def run(self) -> None:
         try:
-            self.__connect()
+            self._connect()
         except socket.error:
-            logging.exception(f"Error while connecting to {self.__m200_address}")
-            return
+            logging.exception(f"Error while connecting to {self._m200_address}")
+            sys.exit(0)
 
         while True:
             try:
-                data = self.__tcp_socket.recv(self.__buffer_size)
+                data = self._tcp_socket.recv(self._buffer_size)
                 if data:
-                    raw_cdr = data.decode(encoding=self.__encoding).strip()
-                    self.__output_queue.put(raw_cdr)
+                    raw_cdr = data.decode(encoding=self._encoding).strip()
+                    self._output_queue.put(raw_cdr)
             except socket.error:
-                host, port = self.__m200_address
+                # потеря соединения, пишем информацию в лог и пытаемся переподключиться
+                host, port = self._m200_address
                 message = f"Connection to {host}:{port} lost. Reconnecting..."
                 logging.debug(message)
-                self.__connected = False
-                self.__tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                while not self.__connected:
+                self._connected = False
+                # попытки переподключений до тех пор пока связь не восстановится
+                while not self._connected:
                     try:
-                        self.__tcp_socket.connect(self.__m200_address)
-                        self.__connected = True
+                        self._setup_socket()
+                        self._tcp_socket.connect(self._m200_address)
+                        self._connected = True
                         message = f"Reconnected to {host}:{port} successfully!"
                         logging.debug(message)
                     except socket.error:
-                        sleep(self.__reconnect_timeout)
+                        # ждём таймаут и снова пытаемся переподключиться
+                        sleep(self._reconnect_timeout)
